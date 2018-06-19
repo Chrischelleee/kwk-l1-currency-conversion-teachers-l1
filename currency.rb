e1bb34fb751f7b53ea18aae86bb71d335d9b8ab1
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-19 00:47:22 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -8,3 +8,23 @@ def eur_to_usd(input)
   ans = input.to_f / 0.86
   return ans
 end
+
+def usd_to_gbp(input)
+  ans = input.to_i * 0.75
+  return ans
+end
+
+def gbp_to_usd(input)
+  ans = input.to_f / 0.75
+  return ans
+end
+
+def usd_to_aud(input)
+  ans = input.to_i * 1.35
+  return ans
+end
+
+def aud_to_usd(input)
+  ans = input.to_f / 1.35
+  return ans
+end
